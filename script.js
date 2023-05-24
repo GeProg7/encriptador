29592e56847f884c7edcab820f25483331cf5dd3
GeProg7 <gsantisteban5@outlook.com>
2023-05-24 12:20:02 -0500

    corrigiendo tolower por toLower

--- script.js
+++ script.js
@@ -17,9 +17,9 @@ function btnEncriptar(){
 
 function encriptar(stringEncriptada){
     let matrizCodigo=[["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    stringEncriptada=stringEncriptada.tolowerCase()
+    stringEncriptada=stringEncriptada.toLowerCase()
 
-    for(let i=0; i < matrizCodigo.length; i++){
+    for(let i = 0; i < matrizCodigo.length; i++){
         if(stringEncriptada.includes(matrizCodigo[i][0])){
             stringEncriptada=stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
         }
